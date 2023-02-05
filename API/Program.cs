ffb53f760ac341fa008f7dea3a7212c3d80f38c5
Shinchoku <tienmp3@gmail.com>
2023-02-05 16:46:43 +0700

    update client package

--- API/Program.cs
+++ API/Program.cs
@@ -27,7 +27,7 @@ app.UseXContentTypeOptions();
 app.UseReferrerPolicy(opt => opt.NoReferrer());
 app.UseXXssProtection(opt => opt.EnabledWithBlockMode());
 app.UseXfo(opt => opt.Deny());
-app.UseCspReportOnly(opt => opt
+app.UseCsp(opt => opt
     .BlockAllMixedContent()
     .StyleSources(s =>
         s.Self().CustomSources("https://fonts.googleapis.com", "sha256-DpOoqibK/BsYhobWHnU38Pyzt5SjDZuR/mFsAiVN7kk="))
